d76053346314f8960fe1e66859bdedbc1b850815
chakaliparashuramudu75-commits <chakaliparashuramudu75@gmail.com>
2026-03-29 00:40:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3880-minimum-absolute-difference-between-two-values/3880-minimum-absolute-difference-between-two-values.java
+++ 3880-minimum-absolute-difference-between-two-values/3880-minimum-absolute-difference-between-two-values.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minAbsoluteDifference(int[] nums) {
         int s=Integer.MAX_VALUE;
-        for(int i=0;i<nums.length;i++)
+       /* for(int i=0;i<nums.length;i++)
         {
             int s1=nums[i];
             for(int j=i+1;j<nums.length;j++)
@@ -14,6 +14,25 @@ class Solution {
                 }
             }
         }
-        return s==Integer.MAX_VALUE?-1:s;
+        return s==Integer.MAX_VALUE?-1:s;*/
+        int fi=-1,la=-1;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==1)
+            {
+                fi=i;
+                if(la!=-1) s=Math.min(s,Math.abs(fi-la));
+            }
+            else if(nums[i]==2)
+            {
+                la=i;
+                if(la!=-1)
+                {
+                    s=Math.min(s,Math.abs(fi-la));
+                }
+            }
+        }
+                return s==Integer.MAX_VALUE?-1:s;
+
     }
 }
