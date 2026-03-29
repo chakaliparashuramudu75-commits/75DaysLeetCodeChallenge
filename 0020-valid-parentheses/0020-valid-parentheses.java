ebd4566e7449ceeea0143b9759aa66765206dcad
chakaliparashuramudu75-commits <chakaliparashuramudu75@gmail.com>
2026-03-29 15:49:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,5 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-        
+        Stack<Character>mahi=new Stack<>();
+        for(char ch:s.toCharArray())
+        {
+            if(ch=='(' || ch=='{' || ch=='[') 
+            {
+                mahi.push(ch);
+            }
+            else 
+            {
+                if(mahi.isEmpty()) return false;
+                char c=mahi.pop();
+                if(((c=='(') && ch!=')') || ((c=='{') && ch!='}') ||((c=='[') && ch!=']')) return false;
+            }
+        }
+        return true;
     }
 }
